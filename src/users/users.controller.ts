200181a7c47ee5948bad1ab3387e2c3004cb5a7b
mauridevcde <mauridevcde@gmail.com>
2025-06-26 21:30:15 -0300

    crud realizada

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { UpdateUserDto } from './dto/udpate-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersServices: UsersService) {}
+
+  @Post()
+  createUser(@Body() newUser: CreateUserDto) {
+    return this.usersServices.createUser(newUser);
+  }
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.usersServices.getUsers();
+  }
+
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersServices.getUser(id);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersServices.deleteUser(id);
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() user: UpdateUserDto,
+  ) {
+    return this.usersServices.updateUser(id, user);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/udpate-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(user: CreateUserDto) {
+    //verificar si el usuario ya existe
+    const existingUser = await this.userRepository.findOne({
+      where: {
+        username: user.username,
+      },
+    });
+
+    if (existingUser) {
+      return new HttpException('usuario ya existe', HttpStatus.CONFLICT);
+    }
+    const newUser = this.userRepository.create(user);
+    return this.userRepository.save(newUser);
+  }
+
+  getUsers() {
+    return this.userRepository.find();
+  }
+
+  async getUser(id: number) {
+    const userFound = await this.userRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!userFound) {
+      return new HttpException('usuario no existe', HttpStatus.NOT_FOUND);
+    }
+    return userFound;
+  }
+
+  async deleteUser(id: number) {
+    // const userFound = await this.userRepository.findOne({
+    //   where: {
+    //     id: id,
+    //   },
+    // });
+    // if (!userFound) {
+    //   return new HttpException('usuario no existe', HttpStatus.NOT_FOUND);
+    // }
+    // return this.userRepository.delete(id);
+
+    const result = await this.userRepository.delete(id);
+    if (result.affected === 0) {
+      throw new HttpException('usuario no existe', HttpStatus.NOT_FOUND);
+    }
+    return result;
+  }
+
+  async updateUser(id: number, user: UpdateUserDto) {
+    const userFound = await this.userRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+    if (!userFound) {
+      return new HttpException('Usuario no existe', HttpStatus.NOT_FOUND);
+    }
+
+    return this.userRepository.update(id, user);
+  }
+}
